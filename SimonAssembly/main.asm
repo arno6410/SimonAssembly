4286d5b47145496e3629358d9771db310f5a58e4
Arno De Haseleer <arno.dehaseleer@gmail.com>
2023-05-13 15:23:26 +0200

    First commit

---
+++ SimonAssembly/main.asm
@@ -0,0 +1,13 @@
+;
+; SimonAssembly.asm
+;
+; Created: 13/05/2023 15:17:08
+; Author : ArnoD
+;
+
+.include "m328pdef.inc"		;Load addressess of IO registers
+
+; Replace with your application code
+start:
+    inc r16
+    rjmp start
