2a12dfc4d260c367e1359d1c3a9f588e36d8df58
Rogier De Nys <rogierdenys@gmail.com>
2023-05-24 14:20:20 +0200

    show sequence op scherm

--- SimonAssembly/display.asm
+++ SimonAssembly/display.asm
@@ -26,92 +26,47 @@ init:
 	sbi ddrb, 5
 	
 main:
-	call show_msg
+	
+	
+	
+	ldi xl,0x00
+	ldi xh,0x01
+	ldi zl, low(2*sequence)
+	ldi zh, high(2*sequence)
+	
+	ldi r16, 0x10
+loop_load_buffer:
+	lpm r17, z+
+	st x+, r17
+	
+	dec r16
+	brne loop_load_buffer
+	
+	call show_buffer
 
 	rjmp main
 
+char0: .db 0b00000, 0b01100, 0b10010, 0b10110, 0b11010, 0b10010, 0b01100, 0
+char1: .db 0b00000, 0b00100, 0b01100, 0b10100, 0b00100, 0b00100, 0b01110, 0
+char2: .db 0b00000, 0b01100, 0b10010, 0b00010, 0b00100, 0b01000, 0b11110, 0
+char3: .db 0b00000, 0b01100, 0b10010, 0b00100, 0b00010, 0b10010, 0b01100, 0
+char4: .db 0b00000, 0b00100, 0b01100, 0b10100, 0b11110, 0b00100, 0b00100, 0
+char5: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b00010, 0b10010, 0b01100, 0
+char6: .db 0b00000, 0b01100, 0b10000, 0b11100, 0b10010, 0b10010, 0b01100, 0
+char7: .db 0b00000, 0b11110, 0b00010, 0b00100, 0b01000, 0b01000, 0b01000, 0
+char8: .db 0b00000, 0b01100, 0b10010, 0b01100, 0b10010, 0b10010, 0b01100, 0
+char9: .db 0b00000, 0b01100, 0b10010, 0b10010, 0b01110, 0b00010, 0b01100, 0
+charA: .db 0b00000, 0b01100, 0b10010, 0b10010, 0b11110, 0b10010, 0b10010, 0
+charB: .db 0b00000, 0b11100, 0b10010, 0b11100, 0b10010, 0b10010, 0b11100, 0
+charC: .db 0b00000, 0b01100, 0b10010, 0b10000, 0b10000, 0b10010, 0b01100, 0
+charD: .db 0b00000, 0b11100, 0b10010, 0b10010, 0b10010, 0b10010, 0b11100, 0
+charE: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b10000, 0b10000, 0b11110, 0
+charF: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b10000, 0b10000, 0b10000, 0
 
-.equ msg_length = 1
-row1:	.db		0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000
-row2:	.db		0b11100, 0b11110, 0b01110, 0b01100, 0b01100, 0b11100
-row3:	.db		0b10010, 0b10000, 0b00100, 0b10010, 0b10010, 0b10010
-row4:	.db		0b10010, 0b11100, 0b00100, 0b10000, 0b10010, 0b10010
-row5:	.db		0b11100, 0b10000, 0b00100, 0b10110, 0b10010, 0b11100
-row6:	.db		0b10010, 0b10000, 0b00100, 0b10010, 0b10010, 0b10010
-row7:	.db		0b10010, 0b11110, 0b01110, 0b01100, 0b01100, 0b10010
-
-msg: .db 0, 1, 2, 3
-charR: .db 0b00000, 0b11100, 0b10010, 0b10010, 0b11100, 0b10010, 0b10010
-charO: .db 0b00000, 0b01100, 0b10010, 0b10010, 0b10010, 0b10010, 0b01100
-charG: .db 0b00000, 0b01100, 0b10010, 0b10000, 0b10110, 0b10010, 0b01100
-charI: .db 0b00000, 0b01110, 0b00100, 0b00100, 0b00100, 0b00100, 0b01110
-charE: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b10000, 0b10000, 0b11110
-
-
-
-show_msg:
-	ldi r18, 7 ; # rows, for loop
-	clr r19	; r19 acts as the row counter
-	ldi r16, 0b0000001 ; initial row selection (row 1)
-loop_show_next_row:
-	push r16 ; save row selection
-	
-	; padding
-	ldi r16, 0b00000
-	ldi r17, 16-msg_length ; # segments to pad
-loop_padding:
-	call show_row_segment5
-	dec r17
-	brne loop_padding
-	
-	ldi zh, high(2*msg)
-	ldi zl, low(2*msg)
-	push zh
-	push zl
-	
-	ldi r17, msg_length
-loop_row:
-
-	pop zl
-	pop zh
-	lpm r20, z+
-	push zh
-	push zl
-	
-	ldi zh, high(2*charR)
-	ldi zl, low(2*charR)
-	lsl r20
-	lsl r20
-	lsl r20
-	add zl, r20
-	brcc carryNotSett
-  	inc zh
- carryNotSett:
-	
-  	add zl, r19
- 	brcc carryNotSet
-  	inc zh
- carryNotSet:
-	
- 	lpm	r16, z ; load segment from program memory
-	call show_row_segment5
-	
-	dec r17
-	brne loop_row
-	
-	pop r16
-	call select_row
-	lsl r16 ; select next row
-	
-	inc r19
-	dec r18
-	brne loop_show_next_row
-ret
-
-
+sequence: .db 0x02, 0x0A, 0x0C, 0x09, 0x00, 0x01, 0x07, 0x0B, 0x03, 0x04, 0x0A, 0x0F, 0x05, 0x0E, 0x01, 0x06
 
 select_row:
-	push r16
+	push r20
 	cbi portb, 3
 
 	shift_reg 7
@@ -125,16 +80,16 @@ select_row:
 	
 	; latch to show row on display
 	sbi portb, 4
-	ldi r16, 255
+	ldi r20, 255
 delayloop1:
-	subi r16, 1
+	subi r20, 1
 	breq enddelayloop1
 	nop
 	rjmp delayloop1
 enddelayloop1:
 	cbi portb, 4
 	
-	pop r16
+	pop r20
 ret
 	
 ; show a row segment (5 pixels wide)
@@ -162,14 +117,94 @@ show_row_segment8:
 	shift_reg 7	
 ret
 
+show_buffer:
+	ldi r21, 7
+	ldi r20, 0b00000001
 
-;Definition of memory address of start and end of charbuffer
-CHARBUFFER_START: .dw	0x0100
-CHARBUFFER_END:  .dw	0x010F
+	send8Row:
+		;send column data
+		ldi r22,16 ;#block
+		ldi yl, low(0x0110) ; end of charbuffer (0x0100 + 16)
+		ldi yh, high(0x0110)
+		blockloop:	
+			ldi zh, high(2*char0)
+			ldi zl, low(2*char0)
+			
+			clr r6
+			add zl,r21 ;+(rownumber-1)
+			adc zh, r6 ;(adc is add with carry or zh + r6 + carry bit)
 
-CharTable:
-.db 0b00000, 0b01100, 0b10010, 0b10010, 0b10010, 0b10010, 0b01100, 0b00000 ;0
-.db 0b00000, 0b00100, 0b01100, 0b10100, 0b00100, 0b00100, 0b11111, 0b00000 ;1
+			ld r23,-y
+
+			ldi r19,8
+			loop:
+				add zl,r23
+				adc zh,r6
+				dec r19
+			brne loop
+
+			lpm
+			ldi r16,5
+			
+
+			blockcolloop:
+				;send 5 bits of loaded byte to the screen
+				cbi portb,3
+				ror r0
+				brcc carryis0
+					sbi portb,3
+				carryis0:
+				cbi portb,5
+				sbi portb,5
+
+				dec r16
+				brne blockcolloop
+
+			dec r22
+			brne blockloop
+
+		ldi r19,8
+		clc
+
+		rowloop:
+				cbi portb,3 ;init pb=0
+			ror r20
+			brcc carryis1
+				sbi portb,3
+			carryis1:
+			cbi portb,5
+			sbi portb,5 ;create rising edge of pb5 to shift
+
+			dec r19 ;loop 8 times
+			brne rowloop
+
+;	rcall select_row
+
+		rcall enable
+		dec r21
+		tst r20
+		brne send8row
+		
+ret
+
+enable:
+	sbi portb,4
+	rcall delay
+	cbi portb,4
+ret
+
+delay:
+	ldi r16,0xff
+	delayloop1_:
+		nop
+		ldi r17,4
+		delayloop2:
+			nop
+			dec r17
+			brne delayloop2
+		dec r16
+		brne delayloop1_
+ret
 
 COMB_ADDRESS: .dw 0x0500 ;?
 
