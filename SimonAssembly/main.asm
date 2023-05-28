b16d0579f7282cac6385e01ef52a476a25f3147e
Arno De Haseleer <arno.dehaseleer@gmail.com>
2023-05-28 15:54:00 +0200

    nu wordt combi langer

--- SimonAssembly/main.asm
+++ SimonAssembly/main.asm
@@ -5,20 +5,23 @@
 ; Author : Arno, Rogier
 ;
 
-.include "m328pdef.inc"		;Load addressess of IO registers
-
-; boot
+; origin
 .org 0x000
 rjmp init
 .org 0x0020
 rjmp Timer1OverflowInterrupt
 
+.include "m328pdef.inc"		;Load addresses of IO registers
+;.include "test.asm"
+
 .macro shift_reg
-	sbrc r16, @0 ; bit # of 16 to shift into register
-	sbi portb, 3
-	cbi portb, 5
-	sbi portb, 5
-	cbi portb, 3
+	cbi portb,3
+	ror @0
+	brcc carry_cleared
+	sbi portb,3
+carry_cleared:
+	cbi portb,5
+	sbi portb,5
 .endmacro
 
 init:
@@ -26,7 +29,7 @@ init:
 	sbi ddrb, 3
 	sbi ddrb, 4
 	sbi ddrb, 5
-
+	
 	;Configure output pin PC2 (LED1)
 	sbi ddrc,2					;Pin PC2 is an ouput, set to 1
 	sbi portc,2					;Output Vcc => LED2 is turned off!
@@ -84,30 +87,150 @@ init:
 	st Z+, r16
 	;...
 	;First correct one put into r24
-	ldi yh, high(COMB_ADDRESS)
-	ldi yl, low(COMB_ADDRESS)
-	ld	r24,Y+
+	ldi xh, high(2*sequence)
+	ldi xl, low(2*sequence)
+	ld	r24,X+
 	;Put combination size in r25
 	ldi r25,0x01
-	mov r26,r25 ;r26 will be used to keep track of the combination
+	mov r6,r25 ;r6 will be used to keep track of the combination
+	
+	mov r18, r6
+loop_seq:
+	
+	ldi yl, low(0x0100)
+	ldi yh, high(0x0100)
+	ldi zl, low(2*COMB_ADDRESS)
+	ldi zh, high(2*COMB_ADDRESS)
+	
+	ldi r16, 0x10
+loop_load_buffer:
+	lpm r17, z+
+	
+	mov r19, r16
+	subi r19, 0x10
+	neg r19
+	sub r19, r18
+	brlt noPad
+	
+	ldi r21, 0x10 ; char 0x10 is an empty segment
+	st y, r21
+	rjmp skipp
+noPad:
+	st y, r17
+skipp:
+
+	adiw y, 1
+	dec r16
+	brne loop_load_buffer
+	
 	
 main:
+	ldi r18, 0x01 ; r18 is the 'level' counter
+
+	
+loop_show:
+	call show_buffer
+	
+	; add here the condition of when to go to the next level
+	rjmp loop_show
 	
 	rjmp main
+	
+char0: .db 0b00000, 0b01100, 0b10010, 0b10110, 0b11010, 0b10010, 0b01100, 0
+char1: .db 0b00000, 0b00100, 0b01100, 0b10100, 0b00100, 0b00100, 0b01110, 0
+char2: .db 0b00000, 0b01100, 0b10010, 0b00010, 0b00100, 0b01000, 0b11110, 0
+char3: .db 0b00000, 0b01100, 0b10010, 0b00100, 0b00010, 0b10010, 0b01100, 0
+char4: .db 0b00000, 0b00100, 0b01100, 0b10100, 0b11110, 0b00100, 0b00100, 0
+char5: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b00010, 0b10010, 0b01100, 0
+char6: .db 0b00000, 0b01100, 0b10000, 0b11100, 0b10010, 0b10010, 0b01100, 0
+char7: .db 0b00000, 0b11110, 0b00010, 0b00100, 0b01000, 0b01000, 0b01000, 0
+char8: .db 0b00000, 0b01100, 0b10010, 0b01100, 0b10010, 0b10010, 0b01100, 0
+char9: .db 0b00000, 0b01100, 0b10010, 0b10010, 0b01110, 0b00010, 0b01100, 0
+charA: .db 0b00000, 0b01100, 0b10010, 0b10010, 0b11110, 0b10010, 0b10010, 0
+charB: .db 0b00000, 0b11100, 0b10010, 0b11100, 0b10010, 0b10010, 0b11100, 0
+charC: .db 0b00000, 0b01100, 0b10010, 0b10000, 0b10000, 0b10010, 0b01100, 0
+charD: .db 0b00000, 0b11100, 0b10010, 0b10010, 0b10010, 0b10010, 0b11100, 0
+charE: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b10000, 0b10000, 0b11110, 0
+charF: .db 0b00000, 0b11110, 0b10000, 0b11100, 0b10000, 0b10000, 0b10000, 0
+empty: .db 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0b00000, 0
 
-	/*cli*/
+sequence: .db 0x02, 0x0A, 0x0C, 0x04, 0x00, 0x01, 0x07, 0x0B, 0x03, 0x04, 0x0A, 0x0F, 0x05, 0x0E, 0x01, 0x06
+
+show_buffer: ; uses r1, r2, r16, 17, 18, 21
+	push r16
+	push r17
+	push r18
+	push r21
 	
-	/*call show_msg
+	ldi r17, 0b00000001
 	
-	ldi r20, 0b11100101
+	ldi r16, 7
+loop_row:
+	ldi yl, low(0x0110) ; end of charbuffer (0x0100 + 16)
+	ldi yh, high(0x0110)
 	
-	call lfsr_r20
+	ldi r18, 16
+loop_block:	
+	ldi zh, high(2*char0)
+	ldi zl, low(2*char0)
 	
-loop:
-	call show_r20
-	rjmp loop*/
+	clr r1
+	add zl, r16
+	adc zh, r1
 	
+	ld r2, -y
+	lsl r2
+	lsl r2
+	lsl r2
+	add zl, r2 
+	adc zh, r1
+
+	lpm ; loads (z) into r0
 	
+	ldi r21,5
+loop_segment5:
+	shift_reg r0
+	dec r21
+	brne loop_segment5
+
+	dec r18
+	brne loop_block
+
+	clc
+	ldi r21,8
+loop_select_row:
+	shift_reg r17
+	dec r21
+	brne loop_select_row
+
+; enter into display
+
+	sbi portb,4
+	
+	ldi r21,0xff
+loop_delay:
+	nop
+	dec r21
+	brne loop_delay
+	
+	cbi portb,4
+	
+	dec r16
+;	tst r17
+	brne loop_row
+	
+	pop r21
+	pop r18
+	pop r17
+	pop r16
+	
+ret
+
+
+
+
+
+
 CheckButtons:
 
 	ldi r20, 0b11110111			;low,high,high,high to see the first row
@@ -212,24 +335,6 @@ K0Pressed:
 ;==========To avoid Relative Branch Out of Reach error==========
 Jump2C:
 	jmp Correct
-;========================
-DELAY:
-	push r16
-	push r17
-	LDI R16,0xFF
-	Delayloop1_:
-		NOP
-		LDI R17,4
-		DelayLoop2:
-			NOP
-			DEC R17
-			BRNE DelayLoop2
-		DEC R16
-		BRNE DelayLoop1_
-	pop r17
-	pop r16
-RET
-;========================
 
 K9Pressed:
 	cpi	r24,0x09 ;Compare with immediate
@@ -275,20 +380,19 @@ KOtherPressed:
 	rjmp NotCorrect
 
 nokeyspressed:
-	
-	ret ;from rcall CheckButtons
+	rjmp finishCheckButtons
 
 Correct:
 	; Jump here when correct button is pressed
 
 	; Load next correct combination from memory
-	ld r24,Y+
-	dec r26	
+	ld r24,X+
+	dec r6	
 	breq Win ;Reset if combination finished
 
 	/*cbi portc,2*/
 
-	ret ;from rcall CheckButtons
+	rjmp finishCheckButtons
 
 NotCorrect:
 	ldi r25,0x01 ;Reset combination to length = 1
@@ -301,47 +405,27 @@ Win:
 
 Reset:
 	;When wrong combination 
-	ldi yh, high(COMB_ADDRESS)
-	ldi yl, low(COMB_ADDRESS)
-	ld	r24,Y+
-	mov r26,r25
-
+	ldi xh, high(COMB_ADDRESS)
+	ldi xl, low(COMB_ADDRESS)
+	ld	r24,X+
+	mov r6,r25
+	
+finishCheckButtons:
 ret ;from rcall CheckButtons
 
 
 
 
-
-
-select_row:
-	push r16
-	cbi portb, 3
-
-	
-	; latch to show row on display
-	sbi portb, 4
-	ldi r16, 255
-delaylop1:
-	subi r16, 1
-	breq enddelayloop1
-	nop
-	rjmp delaylop1
-enddelayloop1:
-	cbi portb, 4
-	
-	pop r16
-ret
-	
-
-
 Timer1OverflowInterrupt:
+	push r18
 	push r16
 	push r17
+	
 
-	IN R0,PINB					;Put value of PINB in R0 (entire byte)
-	BST R0,0	;Copy PB0 (bit 0 of PINB) to the T flag (single bit)
+	in r0,pinb					;Put value of PINB in R0 (entire byte)
+	bst r0,0	;Copy PB0 (bit 0 of PINB) to the T flag (single bit)
 	;The switch is high if the T flag is cleared
-	BRTC EasyDifficulty				;Branch of the T flag is cleared
+	brtc EasyDifficulty				;Branch of the T flag is cleared
 
 HardDifficulty:
 	;===1.666Hz: 56161, prescaler 1024====
@@ -364,22 +448,42 @@ OFContinue:
 	pop r16
 
 	sbi pinc,2 ;Flips the value
-	;sbi portc,3 ;Turn off LED 2
+	sbi portc,3 ;Turn off LED 2
 	
 	sbis portc,2 ; only check buttons on rising edge
 	rcall CheckButtons
 
-	reti
+	pop r18
+	mov r18, r6
+loop_seq2:
+	
+	ldi yl, low(0x0100)
+	ldi yh, high(0x0100)
+	ldi zl, low(2*sequence)
+	ldi zh, high(2*sequence)
+	
+	ldi r16, 0x10
+loop_load_buffer2:
+	lpm r17, z+
+	
+	mov r19, r16
+	subi r19, 0x10
+	neg r19
+	sub r19, r18
+	brlt noPad2
+	
+	ldi r21, 0x10 ; char 0x10 is an empty segment
+	st y, r21
+	rjmp skipp2
+noPad2:
+	st y, r17
+skipp2:
+
+	adiw y, 1
+	dec r16
+	brne loop_load_buffer2
 
 
-;Definition of memory address of start and end of charbuffer
-CHARBUFFER_START: .dw	0x0100
-CHARBUFFER_END:  .dw	0x010F
-
-CharTable:
-.db 0b00000, 0b01100, 0b10010, 0b10010, 0b10010, 0b10010, 0b01100, 0b00000 ;0
-.db 0b00000, 0b00100, 0b01100, 0b10100, 0b00100, 0b00100, 0b11111, 0b00000 ;1
-
+reti
+	
 COMB_ADDRESS: .dw 0x0500 ;?
-
-
